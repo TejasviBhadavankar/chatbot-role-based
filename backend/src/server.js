73046f66e81c3117f4f4dc365aae86fec9172db0
TejasviBhadavankar <tejasvibhadavankar7@gmail.com>
2026-01-13 12:18:10 +0530

    Project completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,9 @@ import connectDB from "./config/db.js";
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
+// server.js is inside backend/src
+const __dirname = path.resolve("..");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
